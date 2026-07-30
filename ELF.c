d8ce867492a52c32d222fc689beb49b2149dd1f0
Bandar12123 <bandarhatim11@gmail.com>
2026-07-30 11:40:56 +0300

    add lib and main func that contine (argc, argv)

--- ELF.c
+++ ELF.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <elf.h> /* Uses system ELF definition structs and constants */
 
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("Usage: %s <elf_file>\n", argv[0]);
+        return 1;
+    }
+}
